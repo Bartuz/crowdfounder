db691f126a266bee4cf2d8ad622dba81740eaa08
Bartuz <iamnotawowplayer@gmail.com>
2013-07-17 12:59:45 -0400

    Init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Crowfounder::Application.config.secret_token = '678efa75cd0de2029a58bab60cbc93376883e80675b5346a410c0513aaa362c8870152b92d5d8d84a8aef2f15720afb46fa55b34aec7b48fe3023b7b56bd24a5'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+Crowfounder::Application.config.session_store :cookie_store, key: '_crowfounder_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# Crowfounder::Application.config.session_store :active_record_store
