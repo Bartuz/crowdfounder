f152566f5c9e8df7b6f7d88b4961583ffe4f29fc
Bartuz <iamnotawowplayer@gmail.com>
2013-07-17 17:19:54 -0400

    Tests to showpage projects

--- test/integration/project_flows_test.rb
+++ test/integration/project_flows_test.rb
@@ -13,6 +13,9 @@ class ProjectFlowsTest < ActionDispatch::IntegrationTest
   	assert page.has_content?('Project 1')
     assert page.has_content?('Project 2')
     assert page.has_content?('Project 3')
+    click_link 'Project 1'
+    assert_equal project_path(project_1), current_path
+    assert find('h1:first').has_content? project1.title
   end
 
   test "navigation" do 
