cbd905237b58780e5f6830fdf275cbd8ea493f95
Bartuz <iamnotawowplayer@gmail.com>
2013-07-17 14:12:25 -0400

    Seeding db

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,8 +5,18 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
-include 'faker'
+require 'faker'
 
 User.destroy_all
 Project.destroy_all
 
+goals = Array.new
+
+99.times do
+	goals << Random.rand(100..99999)
+end
+
+u = User.create!(first_name: Faker::Name.first_name, last_name: Faker::Name.last_name, email: Faker::Internet.email, password: "password")
+3.times do 
+	u.projects.create(title: Faker::Name.title, teaser: Faker::Lorem.sentence(15), description: Faker::Lorem.sentences(8), goal: goals.sample)
+end
