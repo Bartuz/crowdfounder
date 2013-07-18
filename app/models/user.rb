9bd801869a101438b67b384237110dfbd051a23c
Bartuz <iamnotawowplayer@gmail.com>
2013-07-18 18:03:09 -0400

    Adds associations with pledges for rest models

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,6 @@ class User < ActiveRecord::Base
   authenticates_with_sorcery!
   attr_accessible :first_name, :last_name, :email, :password
   has_many :projects
-
+  has_many :pledges
   validates_presence_of :first_name, :last_name ,:email, :password
 end
